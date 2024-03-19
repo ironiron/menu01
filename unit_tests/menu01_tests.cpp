bd2e788a8b85066c9fb4d9f4884a3cb918edfd96
ironiron <raf.mazurkiewicz@op.pl>
2024-03-19 19:54:40 +0100

    Struct with menus is working!

--- unit_tests/menu01_tests.cpp
+++ unit_tests/menu01_tests.cpp
@@ -13,79 +13,135 @@
 //#define CATCH_CONFIG_MAIN
 //#include "catch.hpp"
 
-
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
 
+ class menupage
+ {
 
+ };
 
-template<auto M>
+struct item
+{
+
+    item(const std::string_view (&n)) : name(n)
+    {}
+   const  std::string_view name;
+    int * child;
+};
+
+namespace internal
+{
+
+template<std::size_t N, typename F, std::size_t... I>
+const std::array<item, N> construct(F&& func, std::index_sequence<I...>)
+{
+    return std::array<item, N>{ { (static_cast<void>(I), func(I))... } };
+}
+
+template< std::size_t N, typename F>
+const std::array<item, N> construct(F&& func)
+{
+    return construct<N>(std::forward<F>(func), std::make_index_sequence<N>());
+}
+
+}
+
+template<auto M, typename  T_Par = const std::nullptr_t>
 struct menu_list1
 {
-    menu_list1(const std::string (&s)[M]) :
-            items(
-            { [s]() -> std::array<std::string, (M)>
-            {
-                return std::to_array<const std::string>(s);
-            }() })
+    menu_list1(const std::string_view (&name), const std::string_view (&s)[M], T_Par &upper_level = nullptr) :
+            _name(name),
+                    _items(
+                    { internal::construct<M>(
+
+                    [s](int i) -> const item
+                    {
+                        return item(s[i]);
+                    }) }),
+            parent(upper_level)
     {
-        //const std::array<int,(sizeof...(nodes))> items={names...};
-        //std::cout<<"aaaaaaa ="<<items[0]<<std::endl;
-        std::cout << "aaaaaaa =" << items[0] << "  ads " << items[1]
-                << std::endl;
     }
-//    item* parent = nullptr;
-    //const std::array<item,sizeof(nodes)> items={nodes...};
-    const std::array<std::string, (M)> items;
-    const std::string name;
+    const std::string_view _name;
+    const std::array<item, (M)> _items;
+    const T_Par* parent;
+    const  std::string_view  operator [] (int i) const {return _items[i].name;}
+    const  std::string_view & operator [] (int i) {return _items[i].name;}
 };
 
 template <class T>
 class Menu01
 {
 public:
-    Menu01(T root_page) : list(root_page)
+    Menu01(T &root_page) : _list(root_page)
     {
-        current_index=0;
+     //   _current_index=0;
     }
     virtual ~Menu01(){}
     void MoveDown(void)
     {
-        current_index++;
+        _current_index++;
     }
-    std::string CurrentItemStr(void)
+    const std::string_view CurrentItemStr(void)
     {
-        return list.items[current_index];
+//        return "";
+        return _list._items[_current_index].name;
     }
     unsigned int  CurrentItemInd(void)
     {
-        return current_index;
+        return _current_index;
     }
 
+//    bool GoIn(void)
+//    {
+//        if(_list._items[_current_index].child != nullptr)
+//            _current_index=0;
+//
+//        return _current_index;
+//    }
+
 private:
-    T list;
-    unsigned int current_index=0;
+    T _list;
+    unsigned int _current_index=0;
+    //unsigned int parent_index=0;
 };
 
-TEST_CASE( "After initialization callback should be called with index of active item")
+//TODO
+
+TEST_CASE( "After initialization active item should be 0 (or the first in list)")
 {
-    menu_list1 main_page=menu_list1({"item1","second item"});
+    menu_list1 main_page=menu_list1("main page",{"item1","second item"});
     Menu01 menu=Menu01(main_page);
     CHECK(menu.CurrentItemStr()==std::string("item1"));
     CHECK(menu.CurrentItemInd()==0);
 }
 
-TEST_CASE( "Moves down the list")
-{
-    menu_list1 main_page=menu_list1({"item1","second item"});
-    Menu01 menu=Menu01(main_page);
-    menu.MoveDown();
-    CHECK(menu.CurrentItemStr()==std::string("second item"));
-    CHECK(menu.CurrentItemInd()==1);
-}
+//TEST_CASE( "After initialization callback should be called with index of active item")
+//{
+//    menu_list1 main_page=menu_list1("main page",{"item1","second item"});
+//    Menu01 menu=Menu01(main_page);
+//    CHECK(menu.CurrentItemStr()==std::string("item1"));
+//    CHECK(menu.CurrentItemInd()==0);
+//}
 
+//TEST_CASE( "Moves down the list")
+//{
+//    menu_list1 main_page=menu_list1("main page",{"item1","second item"});
+//    Menu01 menu=Menu01(main_page);
+//    menu.MoveDown();
+//    CHECK(menu.CurrentItemStr()==std::string("second item"));
+//    CHECK(menu.CurrentItemInd()==1);
+//}
 
+//TEST_CASE( "goes into sublist")
+//{
+//    menu_list1 main_page=menu_list1("main page",{"item1","second item"});
+//    Menu01 menu=Menu01(main_page);
+//    menu.MoveDown();
+//    CHECK(menu.CurrentItemStr()==std::string("second item"));
+//    CHECK(menu.CurrentItemInd()==1);
+//}
 
 //int main()
 //{
